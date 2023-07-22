24c6e886114e0004548a529c0f0b2cb63f4356b1
tokiimugi <tokiimugi@users.noreply.github.com>
2023-07-22 14:35:42 -0700

    got rid of daemon

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_socketio import SocketIO
 import Adafruit_DHT
 import time
 import datetime
+import threading
 
 
 app = Flask(__name__)
@@ -16,9 +17,11 @@ temperature = 0.0
 humidity = 0.0
 current_time = datetime.datetime.now()
 
+sensor_thread_running = True
+
 def read_sensor():
-    global temperature, humidity, current_time
-    while True:
+    global temperature, humidity, current_time, sensor_thread_running
+    while sensor_thread_running:
         # Read temperature and humidity data from the sensor
         new_humidity, new_temperature = Adafruit_DHT.read_retry(sensor, gpio_pin)
         
@@ -31,15 +34,20 @@ def read_sensor():
         'temperature': temperature,
         'humidity': humidity,
         'time' : current_time
-    }
+        }
         socketio.emit('update_data', data, namespace='/data')
         # Wait for some time before taking the next reading (e.g., 2 seconds)
         time.sleep(2)
 
-import threading
-sensor_thread = threading.Thread(target=read_sensor)
-sensor_thread.daemon = True
-sensor_thread.start()
+
+
+@app.route('/api/data')
+def get_data():
+    data = {
+        'temperature': temperature,
+        'humidity': humidity
+    }
+    return jsonify(data)
 
 @app.route('/')
 def index():
@@ -47,4 +55,11 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    sensor_thread = threading.Thread(target=read_sensor)
+    sensor.thread.start()
+
+    try:
+        app.run(host='0.0.0.0', port=5000)
+    finally:
+        sensor_thread_running = False
+        sensor_thread.join()
