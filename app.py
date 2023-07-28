d3301afe134e1719d808e260327c6bec29a246a9
tokiimugi <tokiimugi@users.noreply.github.com>
2023-07-27 18:24:48 -0700

    cleaned up and have separate socket call

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 from flask_socketio import SocketIO
 import Adafruit_DHT
 import time
 import datetime
 import threading
-import signal
-import sys
+
 
 
 app = Flask(__name__)
@@ -20,28 +19,38 @@ humidity = 0.0
 current_time = datetime.datetime.now()
 
 sensor_thread_running = True
+connected_clients = set()
 
-@socketio.on('update_data', namespace='/data')
+
 def read_sensor():
     global temperature, humidity, current_time, sensor_thread_running
     while sensor_thread_running:
         # Read temperature and humidity data from the sensor
-        new_humidity, new_temperature = Adafruit_DHT.read_retry(sensor, gpio_pin)
+        new_humidity, new_temperature = Adafruit_DHT.read_retry(sensor, gpio_pin, delay_seconds=0.1)
         
         # Update the global variables only if the data retrieval was successful
         if new_humidity is not None and new_temperature is not None:
             temperature = new_temperature
             humidity = new_humidity
             current_time = datetime.datetime.now()
-        data = {
-        'temperature': temperature,
-        'humidity': humidity
-        }
-        socketio.emit('update_data', data, namespace='/data')
+        
         # Wait for some time before taking the next reading (e.g., 2 seconds)
         time.sleep(2)
 
+def background_task():
+    while sensor_thread_running:
+        if connected_clients:
+            data = {'temperature':temperature, 'humidity': humidity}
+            socketio.emit('update_data', data, '/data')
+        socketio.sleep(2)
 
+@socketio.on('connect')
+def handle_connect():
+    connected_clients.add(request.sid)
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    connected_clients.remove(request.sid)
 
 @app.route('/api/data')
 def get_data():
@@ -61,16 +70,9 @@ if __name__ == '__main__':
     sensor_thread = threading.Thread(target=read_sensor)
     sensor_thread.start()
 
-    
+    socketio.start_background_task(target=background_task)
     try:
         socketio.run(app,host='0.0.0.0', port=5000)
     finally:
         sensor_thread_running = False
         sensor_thread.join()
-        def shutdown_server(signum, frame):
-            print("Shutting down the server...")
-            socketio.stop()
-            print("Server shutdown complete.")
-            sys.exit(0)
-
-        signal.signal(signal.SIGINT, shutdown_server)
